765d01c2b7497d63280d1cb452ab0edf172b5efc
non-binary computer <transnonymous@gmail.com>
2015-02-06 20:10:51 -0500

    Tests and cleans up Blocker

--- blocker.rb
+++ blocker.rb
@@ -1,27 +1,37 @@
 require "./user"
 
-# example tweet
-tweet_text = "+b @butthead +br @abuser +r @spammer random+word @cool_dude"
+class Blocker
+  attr_reader :users
 
-split_tweet = tweet_text.split
+  def initialize(tweet_text)
+    @users = []
 
-users = []
-
-split_tweet.each do |word|
-  $modes ||= ""
-
-  case word
-  when /^\+/ # a mode string
-    $modes = word
-  when /^@/ # a user
-    users << User.new(word, $modes)
-    $modes = nil
+    parse_tweet(tweet_text)
   end
-end
 
-users.each do |u|
-  puts
-  puts "#{u.handle}'s modes: #{u.modes}"
-  puts "#{u.handle} is blocked." if u.blocked?
-  puts "#{u.handle} is reported." if u.reported?
+  def puts_statuses
+    @users.each do |u|
+      puts
+      puts "#{u.handle}'s modes: #{u.modes}"
+      puts "#{u.handle} is blocked." if u.blocked?
+      puts "#{u.handle} is reported." if u.reported?
+    end
+  end
+
+  def parse_tweet(tweet_text)
+    modes = nil
+    split_tweet = tweet_text.split
+
+    split_tweet.each do |word|
+      modes ||= ""
+
+      case word
+      when /^\+/ # a mode string
+        modes = word
+      when /^@/ # a user
+        @users << User.new(word, modes)
+        modes = nil
+      end
+    end
+  end
 end
